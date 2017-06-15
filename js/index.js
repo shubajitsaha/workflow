0b892828d90e79dab6fee7f77e04443bd22501d4
Shubajit Saha <shsaha@microsoft.com>
2017-06-15 13:59:15 +0530

    Making page red

--- js/index.js
+++ js/index.js
@@ -5,24 +5,32 @@
  */
 
 ////////////////////////////////////////////////////////////////////////////////
-function ShowQRCode(url){
-    jQuery('#qrcode').qrcode(url);
-};
+// function ShowQRCode(url){
+//     jQuery('#qrcode').qrcode(url);
+// };
 
-function ShowTitle(title){
-    jQuery('#title').html(title);
-};
+// function ShowTitle(title){
+//     jQuery('#title').html(title);
+// };
 
-function OnShowQRCode(){
-    // Get active tab and retrive url and title.
-    browser.tabs.query({ currentWindow: true, active: true }, function(tabs){
-        var url = tabs[0].url;
-        var title = tabs[0].title;
-        ShowQRCode(url); 
-        ShowTitle(title);       
-    });
-};
+// function OnShowQRCode(){
+//     // Get active tab and retrive url and title.
+//     browser.tabs.query({ currentWindow: true, active: true }, function(tabs){
+//         var url = tabs[0].url;
+//         var title = tabs[0].title;
+//         ShowQRCode(url); 
+//         ShowTitle(title);       
+//     });
+// };
 ////////////////////////////////////////////////////////////////////////////////
 
 // Event binding.
-document.addEventListener("pageshow", OnShowQRCode()); 
+function test(){
+	console.log("start coloring")
+	document.body.style.backgroundColor="red";
+	for(i=0;i<10;i++){
+		console.log("hello world");
+	}
+	console.log("success");
+}
+document.addEventListener("pageshow", test()); 
